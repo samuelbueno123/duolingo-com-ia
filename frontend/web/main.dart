fe93d4e87baea2fff1f14682860df47ddf2eb269
Samuel Bueno <samucapercy@gmail.com>
2026-06-24 11:32:17 -0300

    create dart project

---
+++ frontend/web/main.dart
@@ -0,0 +1,10 @@
+import 'package:web/web.dart' as web;
+import 'package:dio/dio.dart';
+
+void main() {
+  final now = DateTime.now();
+  final element = web.document.querySelector('#output') as web.HTMLDivElement;
+  element.textContent =
+      'The time is ${now.hour}:${now.minute} '
+      'and your Dart web app is running!';
+}
